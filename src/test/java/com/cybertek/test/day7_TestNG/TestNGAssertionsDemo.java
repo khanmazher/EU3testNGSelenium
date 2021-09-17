dc2f5a4cf512a762b7f903b886109554ab0c51db
khanmazher <khanmazher@gmail.com>
2021-09-17 17:39:16 +0300

    TestNG Day5

---
+++ src/test/java/com/cybertek/test/day7_TestNG/TestNGAssertionsDemo.java
@@ -0,0 +1,35 @@
+package com.cybertek.test.day7_TestNG;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNGAssertionsDemo {
+
+    @BeforeMethod
+    public void setUp(){
+        System.out.println("Open Browser");
+    }
+    @Test
+    public void test1(){
+        System.out.println("First Assertion");
+        Assert.assertEquals("title", "tiTle");
+
+        System.out.println("Second Assertion");
+        Assert.assertEquals("url", "url");
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals("test2", "test2");
+
+    }
+
+
+    @AfterMethod
+    public void tearDown(){
+        System.out.println("Close browser");
+
+    }
+}
