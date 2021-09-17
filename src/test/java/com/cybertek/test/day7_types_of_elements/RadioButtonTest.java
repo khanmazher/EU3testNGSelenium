032d1ee456d009873b56e1ddd753970c3f41d586
khanmazher <khanmazher@gmail.com>
2021-09-17 19:30:45 +0300

    RadioButtonTest

---
+++ src/test/java/com/cybertek/test/day7_types_of_elements/RadioButtonTest.java
@@ -0,0 +1,26 @@
+package com.cybertek.test.day7_types_of_elements;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class RadioButtonTest {
+
+    @Test
+    public void test1(){
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+        WebElement blueRadioBtn = driver.findElement(By.cssSelector("#blue"));
+        WebElement redRadioBtn = driver.findElement(By.cssSelector("#red"));
+
+        System.out.println("blueRadioBtn.isSelected() = " + blueRadioBtn.isSelected());
+        System.out.println("redRadioBtn.isSelected() = " + redRadioBtn.isSelected());
+
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
