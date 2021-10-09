f82e665dd1b662cc9fba0e78af385a9665e7abfe
Mazher Khan <khanmazher@gmail.com>
2021-10-09 23:12:27 +0300

    More commits made

--- src/test/java/com/cybertek/test/day12_properties_driver_test/AnotherLoginTest.java
+++ src/test/java/com/cybertek/test/day12_properties_driver_test/AnotherLoginTest.java
@@ -1,12 +1,22 @@
 package com.cybertek.test.day12_properties_driver_test;
 
 import com.cybertek.test.TestBase;
+import com.cybertek.utilities.ConfigurationReader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
 public class AnotherLoginTest extends TestBase {
-
     @Test
+    public void test1(){
+        driver.get(ConfigurationReader.get("url"));
 
+        String username = ConfigurationReader.get("driver_username");
+        String password = ConfigurationReader.get("driver_password");
+
+        driver.findElement(By.id("prependedInput")).sendKeys(username);
+        driver.findElement(By.id("prependedInput2")).sendKeys(password+ Keys.ENTER);
+    }
 
 
 }
--- src/test/java/com/cybertek/test/day12_properties_driver_test/LoginTest.java
+++ src/test/java/com/cybertek/test/day12_properties_driver_test/LoginTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
+
+
 public class LoginTest {
 
     @Test
